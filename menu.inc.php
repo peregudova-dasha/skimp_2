2c803f8b2f8be2a0e9d7223b9a982c799ab4bed8
Dasha <dasha.peregudova@gmail.com>
2016-05-30 12:59:32 +0300

    Create menu.inc.php

---
+++ menu.inc.php
@@ -0,0 +1,39 @@
+<div id="navigation">
+          <nav class="navbar navbar-default">
+            <div class="container-fluid">
+
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+
+              </div>
+
+              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                  <li><a href="index.html">Главная</a></li>
+                  <li class="dropdown">
+                    <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Наши услуги<span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                      <li><a href="polygraphy.html">Полиграфическая продукция</a></li>
+                      <li><a href="lfp.html">Широкоформатная печать</a></li>
+                      <li><a href="digital.html">Цифровая печать</a></li>
+                      <li><a href="corp.html">Корпоративная продукция</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="portfolio.html">Примеры наших работ</a></li>
+
+                </ul>
+
+                <ul class="nav navbar-nav navbar-right">
+                  <li><a href="about_us.html">О нас</a></li>
+                  <li><a href="#scheme">Схема работы</a></li>
+                  <li><a href="#mail">Связаться с нами</a></li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>  
